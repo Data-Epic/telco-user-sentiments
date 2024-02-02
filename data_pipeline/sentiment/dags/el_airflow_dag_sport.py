98961d411c21f17ae5062ae133764071437c44bd
LogicAL007 <ayomidemtsh@gmail.com>
2024-02-02 18:41:50 +0100

    included dags for sport and cryptocurrency

--- data_pipeline/sentiment/dags/el_airflow_dag_sport.py
+++ data_pipeline/sentiment/dags/el_airflow_dag_sport.py
@@ -7,20 +7,19 @@ from newsapi import NewsApiClient
 from airflow.providers.mongo.hooks.mongo import MongoHook
 from datetime import datetime, timedelta
 
-
 @dag(
     schedule_interval=None,
     start_date=pendulum.datetime(2024, 1, 1, tz="UTC"),
     catchup=False,
-    tags=['Loading Raw data into Airflow'],
+    tags=['Loading raw sport data into Airflow'],
 )
-def el_news_data_into_mongodb():
+def el_sport_data_into_mongodb():
     @task
     def extract_from_newsapi(q, from_param, to, language, pages):
 
         newsapi = NewsApiClient(api_key='188877b9bd2741479357336cdb5bc761')
         all_articles = []
-        for page in range(pages, pages + 4):
+        for page in range(pages, pages + 5):
             response = newsapi.get_everything(q=q,
                                               from_param=from_param,
                                               to=to,
@@ -29,7 +28,7 @@ def el_news_data_into_mongodb():
             articles = response['articles']
             logging.info(f"Successfully fetched articles for page {page}")
             all_articles.append(articles)
-            if page < pages + 3:
+            if page < pages + 4:
                 logging.info("Waiting 3 seconds before fetching the next page...")
                 time.sleep(3)
         return all_articles
@@ -40,7 +39,7 @@ def el_news_data_into_mongodb():
             hook = MongoHook(mongo_conn_id="mongo_default")
             client = hook.get_conn()
             db = client.trending_data
-            collection = db.raw_electric_vehicle
+            collection = db.raw_sport
             logging.info(f"Connected to MongoDB - {client.server_info()}")
             for article in all_articles:
                 collection.insert_many(article)
@@ -49,9 +48,8 @@ def el_news_data_into_mongodb():
             logging.error(f"Error connecting to or inserting into MongoDB: {e}")
 
 
-    all_articles = extract_from_newsapi("tesla", datetime.today().date() - timedelta(days=1), datetime.today().date(),
+    all_articles = extract_from_newsapi("Formula 1", datetime.today().date() - timedelta(days=30), datetime.today().date(),
                                         "en", 1)
     load_raw_data(all_articles)
 
-
-el_raw_collection_dag_mongodb = el_news_data_into_mongodb()
+el_sport_data_into_mongodb()
